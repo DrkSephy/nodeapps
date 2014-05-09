1120ee8b1cad23a68e7d5303b1a4b2b97de61f82
David Leonard <sephirothcloud1025@yahoo.com>
2014-05-08 21:08:41 -0400

    Fixing errors with delete users method

--- nodetest2/routes/users.js
+++ nodetest2/routes/users.js
@@ -23,15 +23,17 @@ router.post('/adduser', function(req, res){
     });
 });
 
+
 /*
-* DELETE to deleteuser
-*/
-router.delete('/deleteuser/:id', function(req, res){
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
     var db = req.db;
     var userToDelete = req.params.id;
-    db.collection('userlist').removeByID(userToDelete, function(err, result){
-        res.send((result === 1) ? {msg: ''} : { msg: 'error: ' + err })
+    db.collection('userlist').removeById(userToDelete, function(err, result) {
+        res.send((result === 1) ? { msg: '' } : { msg:'error: ' + err });
     });
 });
+
 
 module.exports = router;
