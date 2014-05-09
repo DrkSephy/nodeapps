d954e3f6811b420edf008464bc7ee6ae27b54920
David Leonard <sephirothcloud1025@yahoo.com>
2014-05-08 20:57:49 -0400

    Adding function for deleting users

--- nodetest2/public/javascripts/global.js
+++ nodetest2/public/javascripts/global.js
@@ -12,6 +12,11 @@ $(document).ready(function(){
 
     // Add User button click
     $('#btnAddUser').on('click', addUser);
+
+    // Delete User link click
+    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
+
+
 });
 
 // Functions 
@@ -120,4 +125,43 @@ function addUser(event) {
         alert('Please fill in all fields');
         return false;
     }
+};
+
+// Delete User
+function deleteUser(event) {
+
+    event.preventDefault();
+
+    // Pop up a confirmation dialog
+    var confirmation = confirm('Are you sure you want to delete this user?');
+
+    // Check and make sure the user confirmed
+    if (confirmation === true) {
+
+        // If they did, do our delete
+        $.ajax({
+            type: 'DELETE',
+            url: '/users/deleteuser/' + $(this).attr('rel')
+        }).done(function( response ) {
+
+            // Check for a successful (blank) response
+            if (response.msg === '') {
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
+
+            // Update the table
+            populateTable();
+
+        });
+
+    }
+    else {
+
+        // If they said no to the confirm, do nothing
+        return false;
+
+    }
+
 };
