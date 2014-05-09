a2c86e3dbac33f1f7b59ef9826d0cc91da3240c5
David Leonard <sephirothcloud1025@yahoo.com>
2014-05-08 20:16:52 -0400

    Adding populate methods for user info

--- nodetest2/public/javascripts/global.js
+++ nodetest2/public/javascripts/global.js
@@ -50,4 +50,12 @@ function showUserInfo(event){
     // Returns a new array containing what the function returns
     var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
 
+    // Get the User Object
+    var thisUserObject = userListData[arrayPosition];
+
+    //Populate Info Box
+    $('#userInfoName').text(thisUserObject.fullname);
+    $('#userInfoAge').text(this.UserObject.age);
+    $('#userInfoGender').text(this.UserObject.gender);
+    $('#userInfoLocation').text(this.UserObject.location);
 }
